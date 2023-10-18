a1df2bf170f11604612df88cbe80d2d66befb416
Rennanbarao0 <rennanbezerra07@gmail.com>
2023-10-18 12:23:00 -0300

    feat: add express initial server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log('Testando aplicação')
+import { config } from "dotenv";
+import express from "express";
+
+config();
+
+const app = express();
+
+const port = process.env.PORT || 8000;
+
+app.get("/", (req, res) => {
+  res.send("hello wordl");
+});
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}!`);
+});
