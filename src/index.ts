2d31863a5b13923b8f4676b48ccb933d5489a9ae
Rennanbarao0 <rennanbezerra07@gmail.com>
2023-10-19 14:20:13 -0300

    feat: add UpdateUserController with validations

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,8 @@ import { MongoGetUsersRepository } from "./repositories/get-users/mongo-get-user
 import { MongoClient } from "./database/mongo";
 import { MongoCreateUserRepository } from "./repositories/create-user/mongo-create-user";
 import { CreateUserController } from "./controllers/create-user/create-user";
+import { MongoUpdateUserRepository } from "./repositories/update-user/mongo-update-user";
+import { UpdateUserController } from "./controllers/update-user/update-user";
 
 const main = async () => {
   config();
@@ -35,7 +37,22 @@ const main = async () => {
       body: req.body,
     });
 
-    return res.status(statusCode).send(body);
+    res.status(statusCode).send(body);
+  });
+
+  app.patch("users/:id", async (req, resp) => {
+    const mongoUpdateUserRepository = new MongoUpdateUserRepository();
+
+    const updateUserController = new UpdateUserController(
+      mongoUpdateUserRepository
+    );
+
+    const { body, statusCode } = await updateUserController.handle({
+      body: req.body,
+      params: req.params,
+    });
+
+    resp.status(statusCode).send(body);
   });
 
   const port = process.env.PORT || 8000;
